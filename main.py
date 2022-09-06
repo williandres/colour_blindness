bb829654f8a419d8e9c6c9bb1621b153c779bf4c
Willian Cárdenas <sca.willian@gmail.com>
2022-09-06 13:01:40 -0500

    libraries

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-run():
-  pass
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 
+def run():
+  upload_image(route)
+
+def upload_image():
+  matrix = mpimg.imread(route).tolist()
+  height = len(matriz)
+  width = len(matriz[0])
+  img = []
+  for i in range(alto):
+    row = []
+      for j in range(ancho):
+        r = matriz[i][j][0]
+        g = matriz[i][j][1]
+        b = matriz[i][j][2]
+        rgb = (r, g, b)
+        row.append(rgb)
+    img.append(row)
+  
+  return imagen
 
 if __name__ == '__main__':
   run()
