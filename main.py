eed3c98bc6de93e71596c7ea5ed729cef08b33cc
Willian Cárdenas <sca.willian@gmail.com>
2022-10-12 00:42:52 -0500

    Upload img & pip dependencies

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-run():
-  pass
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+def upload_img(route)->list:
+  mat = mpimg.imread(route).tolist()
+  height = len(mat)
+  width = len(mat[0])
+  img = []
+
+  for i in range(height):
+    row = []
+    for j in range(width):
+      r = mat[i][j][0]
+      g = mat[i][j][1]
+      b = mat[i][j][2]
+      pixel = (r, g, b)
+      row.append(pixel)
+    img.append(row)
+
+  return img
+
+def run():git 
+  upload_img('image.png') # Image route
 
 
 if __name__ == '__main__':
