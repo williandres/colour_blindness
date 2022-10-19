6941f799e69431da2407d0e9c48428c3e2e2ff67
Willian Cárdenas <sca.willian@gmail.com>
2022-10-19 01:08:21 -0500

    Increased quality, trying to delete HUD

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def upload_img(route)->list:
   height = len(mat)
   width = len(mat[0])
   img = []
-
   for i in range(height):
     row = []
     for j in range(width):
@@ -30,25 +29,23 @@ def image_view(img:list)->None:
       row.append([r, g, b])
     mat.append(row)
   plt.imshow(mat)
-  plt.savefig(f'/home/willian/colour_blindness/output/new_img.png')
+  plt.savefig(f'/home/willian/colour_blindness/output/new_img.png', bbox_inches='tight', pad_inches=0, dpi=1200)
 
 ##################---- FILTER ----##################
 def rotate90_img(img:list)->list:
   height = len(img[0])
   width = len(img)
   new_img = []
-
   for i in range(height):
     row = []
     for j in range(width):
       row.insert(0,img[j][i])
     new_img.append(row)
-
   return new_img
 
 ##################---- RUN ----##################
 def run():
-  image_view(rotate90_img(upload_img('/home/willian/colour_blindness/content/tech.png'))) # Image route
+  image_view(rotate90_img(upload_img('/home/willian/colour_blindness/content/art.png'))) # Image route
 
 if __name__ == '__main__':
   run()
